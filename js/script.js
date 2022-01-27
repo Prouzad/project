c803a9821f594325b29e8d5cbcabb1d49e318417
Abdulla <doston.ergashev.97@mail.ru>
2022-01-27 09:06:13 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// alert('Hello')
+
+// const result = confirm("Are you here&");
+
+// console.log(result);
+
+// const answe = +prompt("Vam est 18?", "");
+
+// console.log(typeof(answe));
+
+// const answers = [];
+
+// answers[0] = prompt('Kak vashe imya?');
+// answers[1] = prompt('Kak vashe familiya?');
+// answers[2] = prompt('Skolka vam let?');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someirl.com/${category}/5`);
+
+// const user = "Abdulla";
+
+// alert(`Salom, ${user}`);
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
+
